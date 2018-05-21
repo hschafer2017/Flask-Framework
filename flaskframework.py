a2d2b0581f3e64a7f20fc6022cb4699375878ee3
Haley Schafer <hschafer@umich.edu>
2018-05-21 15:45:45 +0000

    flask with search and multiple page options

--- flaskframework.py
+++ flaskframework.py
@@ -1,4 +1,4 @@
-from flask import Flask, request 
+from flask import Flask, request, render_template 
 import os
 import datetime
 
@@ -6,11 +6,23 @@ app  = Flask(__name__)
 
 @app.route("/")
 def say_hi(): 
-    return "hi there"
+    return render_template("index.html")
+    
+@app.route("/search")
+def do_search(): 
+    return "Search Page"
 
-@app.route("/Photos")
-def photo_page(): 
-    return "<h1>This is the photo page<h1>"
+# @app.route("/Photos")
+# def photo_page(): 
+#     return "<h1>This is the photo page<h1>"
+    
+@app.route("/Photos/<id>")
+def show_photo(id): 
+    return "<h1>This is photo {0}</h1>".format(id)
+    
+@app.route("/Cars/<car>/Image/<num>")
+def show_car(car,num): 
+    return "<h2>You got it! {1}<h2>".format(num,car)
 
 @app.route("/Time")
 def show_time(): 
@@ -21,6 +33,3 @@ def show_time():
 
 if __name__ == "__main__": 
         app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv('PORT', 8080)), debug=True)
-
-
-# 
